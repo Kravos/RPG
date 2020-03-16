0781880d6dc730ab102c87aec9b85fafa10682a4
Kravos <kravos.epvp@gmail.com>
2020-03-16 22:56:45 +0100

    Added Enums
    Changes the code a little 
    Will force push to apply from PC now

--- Program.cs
+++ Program.cs
@@ -5,81 +5,78 @@ namespace RPG
 {
     class Program
     {
-        static void Main(string[] args)
+        public enum CommandType
         {
-            //Menue
-            Console.WriteLine("Welcome to my RPG");
+            Create = 1,
+            Exit = 2
+        }
+        public enum RaceType
+        {
+            Human = 1,
+            Monster = 2,
+            Mystic = 3
+        }
+        static void Main(string[] args)
+        { 
+            //Menu
+            Console.WriteLine(
+                "Welcome to my RPG\n\n ---- Menu ----\n Enter one of the following options:\n'create' - to create a new character.\n'exit' - to leave.");
             
-            
-            Console.WriteLine("---- Menue ----");
-            Console.WriteLine("Enter one of the following options\n");
-            Console.WriteLine("'create' - to create a new character.");
-            Console.WriteLine("'exit' - to leave.\n");
+            var menuInput = Console.ReadLine();
+            CommandType resultCommand;
+            while (Enum.TryParse(menuInput, true, out resultCommand))
+            {
+                menuInput = menuInput.ToLower();
+                Enum.TryParse(menuInput, true, out resultCommand);
+                switch (resultCommand)
+                {
+                    case CommandType.Create:
+                        break;
+                    case CommandType.Exit:
+                        return;
+                    default:
+                        Console.WriteLine("Please use one of the given menu options.");
+                        break;
+                }    
+            }
 
-            var menueInput = Console.ReadLine();
-            if (menueInput.Equals("exit", StringComparison.OrdinalIgnoreCase))
-            {
-                return;
-            }
-            else if (menueInput.Equals("Create", StringComparison.OrdinalIgnoreCase))
-            {
-                
-            }
-            else
-            {
-                Console.WriteLine("Please enter a valid option.");
-                return;
-            }
             
-            //New object 
-            var player = new Character();
+            
+            //New object of Character
+            var player = new Character("", "");
             //Nickname
             Console.WriteLine("Please enter your Nickname:");
             var nicknameInput = Console.ReadLine();
             player.Nickname = nicknameInput;
-
-            //Class
-            Console.WriteLine("\nPlease enter your class! Choose between: Human, Monster and Mystic");
-
-            player.Race = Console.ReadLine();
-            String[] classes = {"Human" , "Monster" , "Mystic"};
             
-            if (player.Race.Equals(classes[0] , StringComparison.OrdinalIgnoreCase))
+            //race
+            Console.WriteLine("Please enter your race. Choose from Human, Monster or Mystic.");
+            var raceInput = Console.ReadLine();
+            raceInput = raceInput.ToUpper();
+            RaceType resultRace;
+            Enum.TryParse(raceInput, true, out resultRace);
+            switch (resultRace)
             {
-                player.Race = classes[0];
-            }
-            else if (player.Race.Equals(classes[1], StringComparison.OrdinalIgnoreCase) )
-            {
-                player.Race = classes[1];
-            }
-            else if (player.Race.Equals(classes[2], StringComparison.OrdinalIgnoreCase))
-            {
-                player.Race = classes[2];
-            }
-            else
-            {
-                Console.WriteLine("\nPlease choose one of the given classes.");
-                return;
+                case RaceType.Human:
+                    player.Race = "Human";
+                    break;
+                case RaceType.Monster:
+                    player.Race = "Monster";
+                    break;
+                case RaceType.Mystic:
+                    player.Race = "Mystic";
+                    break;
+                default:
+                    Console.WriteLine("Please enter a valid race");
+                    return;
             }
             
-            //Standard values
-            player.Dodge = 0;
-            player.Money = 0;
-            player.Strength = 100;
-            player.HealthPoints = 1000;
-            player.DefensePoints = 1000;
-            player.Experience = 0;
-            player.Level = 1;
-           
-            
-            //Outputs
-            /*
-            Console.WriteLine("\nYour Nickname is: "+ player.Nickname);
-            Console.WriteLine("Your character is a "+classInput);
-            Console.WriteLine("Dodge: " + player.Dodge + "\nMoney: " + player.Money + "$" + "\nStrength: " + player.Strength + "\nHP: " + player.HealthPoints + "\nDP: " + player.DefensePoints + "\nLevel: " + player.Level + "(" + player.Experience + "EXP)");
-            */
             player.Creation();
-
+            //New object of Rival
+            var enemy = new Rival("Yo Mama");
+            
+            enemy.RivalInfo();
+            Console.WriteLine("Stage Multiplier set to: " + enemy.Multiplier);
         }
     }
 }    
--- character.cs
+++ character.cs
@@ -7,13 +7,26 @@ namespace RPG
         public string Nickname;
         public string Race;
         public int Money;
-        public int HealthPoints;
+        public int HealthPoints ;
         public int DefensePoints;
-        public int Strength;
+        public int Strength ;
         public int Dodge;
         public int Level;
         public int Experience;
 
+        
+        public Character(string Nickname, string Race)
+        {
+            this.Nickname = Nickname;
+            this.Race = Race;
+            this.Money = 0;
+            this.HealthPoints = 1000;
+            this.DefensePoints = 1000;
+            this.Strength = 100;
+            this.Dodge = 1;
+            this.Level = 1;
+            this.Experience = 0;
+        }
         public void Creation()
         {
             Console.WriteLine("\nYour character information:\n" );
@@ -26,5 +39,6 @@ namespace RPG
             Console.WriteLine("Dodge: "+ Dodge);
             Console.WriteLine("Level: "+ Level + " (Experience: "+ Experience +"%)");
         }
+        
     }
 }
