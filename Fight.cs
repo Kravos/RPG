fdb6a9dc046d87e0b492601b5000ed48191987a9
Kravos <kravos.epvp@gmail.com>
2020-03-21 16:52:13 +0100

    Changed calculation
    Added Dodge and Money

--- Fight.cs
+++ Fight.cs
@@ -3,28 +3,53 @@ using System.Threading;
 
 namespace RPG
 {
-    public interface Fight
+    public class Fight
     {
-
-        public static void fight(int PlayerHP, int RivalHP, int PlayerDP, int RivalDP, int playerStrength, int RivalStrength,
-            int playerDodge, int rivalDodge, string PlayerName, string RivalName)
+        public static void fight(int PlayerHP, int RivalHP, int PlayerDP, int RivalDP, int PlayerStrength,
+            int RivalStrength,
+            int PlayerDodge, int RivalDodge, string PlayerName, string RivalName, int PlayerMoney)
         {
-            PlayerHP = PlayerHP +  PlayerDP / 2;
+            PlayerHP = PlayerHP + PlayerDP / 2;
             RivalHP = RivalHP + RivalDP / 2;
 
-            while (PlayerHP >= 0 && RivalHP >= 0)
+            Random DodgeRand = new Random();
+            while (PlayerHP > 0 && RivalHP > 0)
             {
-                RivalHP = RivalHP - playerStrength;
-                PlayerHP = PlayerHP - RivalStrength;
-                Console.WriteLine("\nUpdated stats:\n Your HP: "+ PlayerHP + "\n Rival HP: " + RivalHP);
+                var RandomDodge = DodgeRand.Next(0, 100);
+                if (RandomDodge > RivalDodge)
+                {
+                    RivalHP = RivalHP - PlayerStrength;
+                    Console.WriteLine("\nUpdated stats:\n Your HP: " + PlayerHP + "\n Rival HP: " + RivalHP);
+                }
+                else
+                {
+                    Console.WriteLine("\nYour Rival dodged the attack.");
+
+                }
+
+                if (RandomDodge > PlayerDodge)
+                {
+                    PlayerHP = PlayerHP - RivalStrength;
+                    Console.WriteLine("\nUpdated stats:\n Your HP: " + PlayerHP + "\n Rival HP: " + RivalHP);
+                }
+                else
+                {
+                    Console.WriteLine("\nYou dodged the attack.");
+                }
+
+
                 if (RivalHP <= 0)
                 {
-                    Console.WriteLine(PlayerName + " defeated " + RivalName +"! GG");
-                } else if (PlayerHP <= 0)
+                    Console.WriteLine(PlayerName + " defeated " + RivalName + "! GG\n");
+                    PlayerMoney += 100;
+                    Console.WriteLine(PlayerName + " earned " + PlayerMoney + "$!");
+                }
+                else if (PlayerHP <= 0)
                 {
                     Console.WriteLine(PlayerName + " got defeated by " + RivalName + "! Noob.");
                 }
-                Thread.Sleep(500); 
+
+                Thread.Sleep(500);
             }
         }
     }
