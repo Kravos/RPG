e53059aac26311f6ec42c88eecfc3f4c1d1252d5
Kravos <kravos.epvp@gmail.com>
2020-03-21 16:51:58 +0100

    Created Interface
    Prolly not neccessary lol

---
+++ IFight.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading;
+
+namespace RPG
+{
+    public interface IFight
+    {
+        public void fight(int PlayerHP, int RivalHP, int PlayerDP, int RivalDP, int PlayerStrength,
+            int RivalStrength,
+            int PlayerDodge, int RivalDodge, string PlayerName, string RivalName, int PlayerMoney);
+    }
+}
